80e33ecec25d75feb70b5bd96247fe10fb0a55b1
Tim O'Shea <tim.oshea753@gmail.com>
2014-08-22 22:10:38 -0400

    theano complex64 fft working

--- python/tfft.py
+++ python/tfft.py
@@ -2,6 +2,9 @@
 from gnuradio import gr, blocks, audio
 import numpy, theano, time
 import theano.tensor as T
+from theano.tensor.fourier import Fourier, fft
+
+ftmp = T.fourier;
 
 class fft(gr.sync_block):
     def __init__(self, N):
@@ -12,10 +15,9 @@ class fft(gr.sync_block):
 
         self.set_output_multiple(N);
         self.N = N
-        import theano.tensor as T
-        import theano
-        x = T.matrix("x")
-        w = theano.shared(numpy.ones(self.N, dtype=theano.config.floatX), name="w")
+        x = T.cmatrix("x")
+        w = theano.shared(numpy.ones(self.N, dtype="complex64"), name="w")
+        #w = theano.shared(numpy.ones(self.N, dtype=theano.config.floatX), name="w")
         self.f = theano.function(
             inputs=[x],
             outputs=[T.fourier.fft(x*w, n=N, axis=1)],
@@ -24,9 +26,15 @@ class fft(gr.sync_block):
             name = "f")
 
     def work(self, input_items, output_items):
-        n = len(input_items[0])/N
+        n = len(input_items[0])/self.N
         for i in range(0,n):
-            output_items[0][N*i:N*(i+1)-1] = self.f(input_items[0][N*i:N*(i+1)-1]);
+
+            inmat = numpy.vstack([input_items[0][self.N*i:self.N*(i+1)]]);
+            omat = self.f(inmat);
+            output_items[0][self.N*i:self.N*(i+1)] = omat[0];
+#            print omat.shape;
+            #output_items[0][self.N*i:self.N*(i+1)-1] = self.f(numpy.vstack([input_items[0][self.N*i:self.N*(i+1)-1]]));
+            #output_items[0][self.N*i:self.N*(i+1)-1] = self.f(numpy.vstack([input_items[0][self.N*i:self.N*(i+1)-1]]));
         return len(output_items[0])
 
 
